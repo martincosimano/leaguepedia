36db03ac0424d29357d9a0be1fd7dbfdafdd0f50
Martin Cosimano <martincosimanodev@gmail.com>
2023-11-05 16:08:44 -0300

    working on the role nav filtering function

--- app/components/ChampionsSection.jsx
+++ app/components/ChampionsSection.jsx
@@ -1,27 +1,11 @@
 "use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 import DropdownRoles from "./DropdownRoles";
 import Card from "./Card";
 
 // Roles navbar
 const ChampionsSection = () => {
-
-    const [selected, setSelected] = useState({
-        all: true,
-        assassins: false,
-        fighters: false,
-        mages: false,
-        marksmen: false,
-        supports: false,
-        tanks: false,
-    });
-
-    function handleSelection(role) {
-        setSelected((prevSelected) => ({
-            ...Object.fromEntries(Object.entries(prevSelected).map(([key]) => [key, key === role])),
-        }));
-    }
 
     // Dropdown menu
     const [active, setActive] = useState(false)
@@ -30,8 +14,19 @@ const ChampionsSection = () => {
         setActive(prevActive => !prevActive)
     }
 
-    // Card display
+
+    const [selected, setSelected] = useState({
+        All: true,
+        Assassin: false,
+        Fighter: false,
+        Mage: false,
+        Marksman: false,
+        Support: false,
+        Tank: false,
+    });
+
     const [apiData, setApiData] = useState([]);
+    const [filteredChampions, setFilteredChampions] = useState([]);
 
     useEffect(() => {
         async function getData() {
@@ -44,14 +39,51 @@ const ChampionsSection = () => {
                 }
                 const res = await data.json();
                 const championsData = Object.values(res.data);
-        
+
                 setApiData(championsData);
+                setFilteredChampions(championsData);
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
         }
         getData();
     }, []);
+
+    function handleSelection(role) {
+        if (role === "All") {
+            setSelected({
+                All: true,
+                Assassin: false,
+                Fighter: false,
+                Mage: false,
+                Marksman: false,
+                Support: false,
+                Tank: false,
+            });
+            setFilteredChampions(apiData);
+        } else {
+            setSelected((prevSelected) => ({
+                ...Object.fromEntries(
+                    Object.entries(prevSelected).map(([key]) => [key, key === role])
+                ),
+            }));
+
+            const filtered = apiData.filter((champion) =>
+                champion.tags.includes(role)
+            );
+            setFilteredChampions(filtered);
+        }
+    }
+
+    const roleButtons = [
+        { role: "All", label: "All" },
+        { role: "Assassin", label: "Assassins" },
+        { role: "Fighter", label: "Fighters" },
+        { role: "Mage", label: "Mages" },
+        { role: "Marksman", label: "Marksmen" },
+        { role: "Support", label: "Supports" },
+        { role: "Tank", label: "Tanks" },
+    ];
 
     return (
         <section className="mt-6 md:my-10">
@@ -62,37 +94,31 @@ const ChampionsSection = () => {
             </div>
             <nav className="hidden md:flex">
                 <ul className="hidden md:flex justify-between w-full list-none">
-                    <li>
-                        <button className={`text-lg font-medium uppercase ${selected.all && 'border-b-2 border-gold-100'}`} onClick={() => handleSelection('all')}>All</button>
-                    </li>
-                    <li>
-                        <button className={`text-lg font-medium uppercase ${selected.assassins && 'border-b-2 border-gold-100'}`} onClick={() => handleSelection('assassins')}>Assassins</button>
-                    </li>
-                    <li>
-                        <button className={`text-lg font-medium uppercase ${selected.fighters && 'border-b-2 border-gold-100'}`} onClick={() => handleSelection('fighters')}>Fighters</button>
-                    </li>
-                    <li>
-                        <button className={`text-lg font-medium uppercase ${selected.mages && 'border-b-2 border-gold-100'}`} onClick={() => handleSelection('mages')}>Mages</button>
-                    </li>
-                    <li>
-                        <button className={`text-lg font-medium uppercase ${selected.marksmen && 'border-b-2 border-gold-100'}`} onClick={() => handleSelection('marksmen')}>Marksmen</button>
-                    </li>
-                    <li>
-                        <button className={`text-lg font-medium uppercase ${selected.supports && 'border-b-2 border-gold-100'}`} onClick={() => handleSelection('supports')}>Supports</button>
-                    </li>
-                    <li>
-                        <button className={`text-lg font-medium uppercase ${selected.tanks && 'border-b-2 border-gold-100'}`} onClick={() => handleSelection('tanks')}>Tanks</button>
-                    </li>
+                    {roleButtons.map((roleButton) => (
+                        <li key={roleButton.role}>
+                            <button
+                                className={`text-lg font-medium uppercase ${selected[roleButton.role] && "border-b-2 border-gold-100"
+                                    }`}
+                                onClick={() => handleSelection(roleButton.role)}
+                            >
+                                {roleButton.label}
+                            </button>
+                        </li>
+                    ))}
                 </ul>
             </nav>
 
-            <div className="mt-6 md:mt-10 flex flex-wrap gap-6 justify-between">
-                {apiData.map((champion, index) => (
-                    <Card key={index} championName={champion.name} championImg={champion.image.full.split('.')[0]} />
+            <div className="mt-6 md:mt-10 flex flex-wrap gap-6 mx-0.5">
+                {filteredChampions.map((champion, index) => (
+                    <Card
+                        key={index}
+                        championName={champion.name}
+                        championImg={champion.image.full.split(".")[0]}
+                    />
                 ))}
             </div>
         </section>
-    )
+    );
 };
 
 export default ChampionsSection;
