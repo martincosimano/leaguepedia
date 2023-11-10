291661e7585d9660717f98693e63bc56fb24d8be
Martin Cosimano <martincosimanodev@gmail.com>
2023-11-09 22:43:57 -0300

    added remotePatterns specified to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['ddragon.leagueoflegends.com']
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'ddragon.leagueoflegends.com'
+            }
+            ]
     }
 }
 
