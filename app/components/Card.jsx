ff97584b7a3df1017ce4bd7f39aeac581910778e
Martin Cosimano <martincosimanodev@gmail.com>
2023-11-09 23:42:42 -0300

    changed card images priority

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -8,7 +8,7 @@ const Card = (props) => {
     return (
         <Link href={`/champion/${props.championUrl}`}>
             <div className="card h-80 w-fit drop-shadow-xl">
-                <Image src={`/champions/${props.championUrl}_0.webp`} alt="" width="220" height="100" />
+                <Image src={`/champions/${props.championUrl}_0.webp`} alt="" width="220" height="100" priority={true} />
                 <button className="card-btn w-full bottom-0 text-lg font-semibold text-white">{props.championName}</button>
             </div>
         </Link>
