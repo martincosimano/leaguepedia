a4050e8f443bb03c1555c67167b3c44ace0a15cc
Martin Cosimano <martincosimanodev@gmail.com>
2023-11-10 19:45:47 -0300

    modified home layout, using grid now

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 const Card = ( {championUrl, championName } ) => {
 
     return (
-        <Link href={`/champion/${championUrl}`}>
-            <div className="card h-80 w-fit">
+        <Link className="col-span-6 sm:col-span-4 md:col-span-3 lg:col-span-2" href={`/champion/${championUrl}`}>
+            <div className="card max-h-80 w-fit">
                 <Image className="card-img" src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${championUrl}_0.jpg`} alt="" width="220" height="100" priority={true} />
                 <button className="card-btn w-full bottom-0 text-lg font-semibold text-white">{championName}</button>
             </div>
