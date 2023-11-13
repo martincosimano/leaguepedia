79cfeb7cf2addd66e75994d2455fea15952205d5
Martin Cosimano <martincosimanodev@gmail.com>
2023-11-13 11:48:26 -0300

    added github link to navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,10 +2,15 @@ import Image from "next/image";
 
 const Navbar = () => {
     return (
-        <header className="my-5">
+        <header className="my-5 flex justify-between items-center">
             <div>
                 <Image src="/logo.png" alt="" className="w-fit" width="30" height="30"/>
-            </div> 
+            </div>
+            <div>
+                <a href="https://github.com/martincosimano" className="opacity-90 hover:opacity-100">
+                    <Image src="/github.svg" alt="" width="30" height="30"/>
+                </a>
+            </div>
         </header>
     )
 }
